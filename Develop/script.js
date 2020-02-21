fb440a1f2ee57e1ca2a0305882141f60c06bf670
Aliagho <ali.bahrami633@yahoo.com>
2020-02-21 14:49:54 +1100

    Password Generator Problem Solved

--- Develop/script.js
+++ Develop/script.js
@@ -12,48 +12,52 @@ function writePassword() {
 
 // Generates password based on user input
 function generatePassword() {
-  return userInput();
-}
-
-function userInput() {
   var passwordLength = window.prompt("Please enter the length of the password - must be between 8 to 128");
   
-  if (passwordLength >= 0 && passwordLength <= 128) {    
-    var hasUpperCase = window.confirm("Do you want to include uppercase letters?");
-    var hasLowerCase = window.confirm("Do you want to include lowercase letters?");
-    var hasNumeric = window.confirm("Do you want to include numerics?");
-    var hasSpecialChar = window.confirm("Do you want to include special characters?");
-    
-    return passwordLength;
-  }
-  else {
-    return window.prompt("Your password length must be less than128 and more than 0!");
-  }
+  while (passwordLength < 8 || passwordLength > 128) {
+    var passwordLength = window.prompt("Your password length must be less than 128 and more than 8!");
+  }    
+  var hasUpperCase = window.confirm("Do you want to include uppercase letters?");
+  var hasLowerCase = window.confirm("Do you want to include lowercase letters?");
+  var hasNumeric = window.confirm("Do you want to include numerics?");
+  var hasSpecialChar = window.confirm("Do you want to include special characters?");    
+  
+  return randomGenerator(passwordLength, hasUpperCase, hasLowerCase, hasNumeric, hasSpecialChar);
 }
 
 // Generates an array of password based on user's inputs
-function randomGenerator(length, hasUpperCase, hasLowerCase, hasNumeric, hasSpecialChar) {
-  var charArray = new Array(length);
+function randomGenerator(passwordLength, hasUpperCase, hasLowerCase, hasNumeric, hasSpecialChar) {
+  var charArray = new Array();
   var initialLength = hasLowerCase + hasNumeric + hasSpecialChar + hasUpperCase;
-  var remainingLength = charArray.length - initialLength;
-  // generates a number for the remaining empty characters of the array
-  var numberFiller = Math.floor((Math.random() * Math.pow(10, remainingLength) + remainingLength));
+  var remainingLength = passwordLength - initialLength;
   
-  // if (hasUpperCase) {
-  //   // push a random uppercase letter
-  // }
-  // if (hasLowerCase) {
-  //   // push a random lowercase letter
-  // }
-  // if (hasNumeric) {
-  //   // push a random integer
-  // }
-  // if (hasSpecialChar) {
-  //   // push a random special character
-  // }  
+  // adds extra numbers to fill the remaining 
+  for (var i = 0; i < remainingLength; i++) {
+    charArray.push(generateRandomLetter(97, 122));
+  }
 
+  // adds user's chosen letters to the array
+  if (hasUpperCase) {
+    // push a random uppercase letter
+    charArray.push(generateRandomLetter(56, 90));    
+  }
+  if (hasLowerCase) {
+    // push a random lowercase letter
+    charArray.push(generateRandomLetter(97, 122));
+  }
+  if (hasNumeric) {
+    // push a random integer
+    charArray.push(generateRandomLetter(48, 57));    
+  }
+  if (hasSpecialChar) {
+    // push a random special character
+    charArray.push(generateRandomLetter(33, 47));
+  }
+  
+  return charArray.join('');
 }
-// Generates random uppercase or lower case letters based on the input. 65-90 for uppercase, 97-122 for lowercase
+
+// Generates random uppercase or lower case letters based on the input. 65-90 for uppercase, 97-122 for lowercase, 48-57 for number, 33-47 for special characters
 function generateRandomLetter(asciiLow, asciiHigh) {
 
   return String.fromCharCode(Math.floor((Math.random() * (asciiHigh - asciiLow)) + asciiLow));
